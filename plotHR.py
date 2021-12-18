74a5bd99bd3cba65aa2a2e84097eb555d4428612
Oliver Kraft <oliver@kraften.net>
2021-12-18 03:01:25 +0100

    Finished assignements

--- plotHR.py
+++ plotHR.py
@@ -2,27 +2,25 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-filename = '4.69e9y0.000152m.txt'
+filename = '10.1e9y0.0152m.txt'
 iDF = pd.read_table(filename, header=13, skipfooter=2, engine='python', delim_whitespace=True)
 
 print(iDF.axes)
 
-cm= 1/2.54
 
 # Plot HR-diagram
 plt.figure(dpi=300)
-plt.subplot(figsize=(12*cm, 12*cm))
 plt.plot(10 ** iDF['logTe'], iDF['logL'], '.k')
 plt.title(filename[:-4])
 plt.grid()
-plt.xlim(9000,3000)
+plt.xlim(7000,2000)
 plt.xlabel('Effective temperature')
 plt.ylabel('Log L/L_sun')
 # Sun properties
 plt.axvline(5772, ymin=0, ymax=1, linestyle='--', color='r')
 plt.axhline(0, xmin=0, xmax=1, linestyle='--', color='r')
 # Testing for exact values
-plt.axvline(8050, ymin=0, ymax=1, linestyle='--', color='b')
-plt.axhline(0.82, xmin=0, xmax=1, linestyle='--', color='b')
+#plt.axvline(8050, ymin=0, ymax=1, linestyle='--', color='b')
+#plt.axhline(0.82, xmin=0, xmax=1, linestyle='--', color='b')
 plt.show()
 
--- plotHRvs.py
+++ plotHRvs.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-filename = ['4.69e9y0.000152m.txt', '4.69e9y0.0152m.txt']
+filename = ['10.1e9y0.0152m.txt', '4.69e9y0.0152m.txt']
 i1DF = pd.read_table(filename[0], header=13, skipfooter=2, engine='python', delim_whitespace=True)
 
 i2DF = pd.read_table(filename[1], header=13, skipfooter=2, engine='python', delim_whitespace=True)
 
 # Plot HR-diagram
 plt.figure(dpi=300)
-plt.plot(10 ** i1DF['logTe'], i1DF['logL'], '.k', label=filename[0][7:-4])
-plt.plot(10 ** i2DF['logTe'], i2DF['logL'], '.b', label=filename[1][7:-4])
+plt.plot(10 ** i1DF['logTe'], i1DF['logL'], '.k', label=filename[0][0:6])
+plt.plot(10 ** i2DF['logTe'], i2DF['logL'], '.b', label=filename[1][0:6])
 plt.legend()
 plt.title(filename[0][:-4] + ' vs ' + filename[1][:-4])
 plt.grid()
